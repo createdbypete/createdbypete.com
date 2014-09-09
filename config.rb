3dbd4ef21d7d41b1143903152f3fce159b092f37
Peter Rhoades <createdbypete@gmail.com>
2014-09-09 13:57:19 +0100

    Add QR code helper and keep 404 page as .html

--- config.rb
+++ config.rb
@@ -11,8 +11,22 @@ end
 activate :directory_indexes
 activate :alias
 
+page "/404.html", directory_index: false
+
 configure :development do
   set :debug_assets, true
   set :analytics, false
   set :site_url, 'http://localhost:4567'
+end
+
+helpers do
+  def qr_code(page_url)
+    qr = RQRCode::QRCode.new(page_url.to_s, size: 10)
+    png = qr.to_img
+    <<-MARKUP.strip
+    <div class="qrcode">
+      <img src="#{png.to_data_url}" alt="#{page_url}">
+    </div>
+    MARKUP
+  end
 end
